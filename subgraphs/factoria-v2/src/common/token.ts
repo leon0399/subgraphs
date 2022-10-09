424cea5f6fec294f727eb8906fb391223f2dfd42
leon0399 <hello@leon0399.ru>
2022-10-09 14:45:02 +0300

    feat(Factoria): store tokens

---
+++ subgraphs/factoria-v2/src/common/token.ts
@@ -0,0 +1,29 @@
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts"
+
+import { BIGINT_ZERO, GENESIS_ADDRESS } from './constants';
+
+import { F0 } from '../../generated/Factoria/F0';
+import { Token, Collection } from '../../generated/schema';
+
+export function getTokenId(collection: Collection, tokenId: BigInt): string {
+  return `${collection.id}/${tokenId.toString()}`
+}
+
+export function createToken(
+  contract: F0,
+  collection: Collection,
+  tokenId: BigInt
+): Token {
+  const tokenStoreId = getTokenId(collection, tokenId)
+
+  const token = new Token(tokenStoreId)
+
+  token.collection = collection.id
+  token.tokenId = tokenId
+  token.tokenURI = contract.tokenURI(tokenId)
+  token.ownerId = GENESIS_ADDRESS
+
+  token.save()
+
+  return token
+}
--- subgraphs/factoria-v2/src/mappings/collection.ts
+++ subgraphs/factoria-v2/src/mappings/collection.ts
@@ -1,19 +1,23 @@
 import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts"
 
-import { F0, Transfer } from '../../generated/Factoria/F0';
-import { Collection } from "../../generated/schema";
+import { F0, Transfer as TransferEvent } from '../../generated/Factoria/F0';
+import { Collection, Token } from "../../generated/schema";
 import { BIGINT_ONE, GENESIS_ADDRESS } from "../common/constants";
+import { createToken, getTokenId } from "../common/token";
 
-export function handleTransfer(event: Transfer): void {
+export function handleTransfer(event: TransferEvent): void {
   const collectionAddress = event.address;
   const fromAddress = event.params.from;
   const toAddress = event.params.to;
   const tokenId = event.params.tokenId;
+  const contract = F0.bind(collectionAddress)
 
-  log.info("Collection: {}, tx: {}, from: {}, to: {}", [collectionAddress.toHex(), event.transaction.hash.toHex(), fromAddress.toHex(), toAddress.toHex()])
+  if (fromAddress.toHex() == GENESIS_ADDRESS && toAddress.toHex() == GENESIS_ADDRESS) {
+    // skip if the transfer is from zero address to zero address
+    return;
+  }
 
   let collection = Collection.load(collectionAddress.toHex())
-
   if (collection == null) {
     log.critical('Collection not found!', [])
     return;
@@ -24,7 +28,20 @@ export function handleTransfer(event: Transfer): void {
     collection.tokenCount = collection.tokenCount.plus(BIGINT_ONE)
   }
 
-  collection.transferCount = collection.transferCount.plus(BIGINT_ONE)
+  if (toAddress.toHex() == GENESIS_ADDRESS) {
+    // burn an existing token
+    collection.tokenCount = collection.tokenCount.minus(BIGINT_ONE)
+  }
 
+  const tokenStoreId = getTokenId(collection, tokenId)
+  let token = Token.load(tokenStoreId)
+  if (token == null) {
+    token = createToken(contract, collection, tokenId)
+  }
+
+  token.ownerId = toAddress.toHex()
+  token.save()
+
+  collection.transferCount = collection.transferCount.plus(BIGINT_ONE)
   collection.save()
 }
